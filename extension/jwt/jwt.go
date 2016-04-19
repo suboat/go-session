64856927bb5e3e8213df358470105f9edb3a039b
WindomZ <zwdsdtc@gmail.com>
2016-04-19 16:24:45 +0800

    feat: jwt

--- extension/jwt/jwt.go
+++ extension/jwt/jwt.go
@@ -1,1 +1,37 @@
 package gosession
+
+import (
+	"github.com/WindomZ/go-jwt"
+	"net/http"
+)
+
+type JWT struct {
+}
+
+func NewJWT(path string) (*JWT, error) {
+	j := &JWT{}
+	if err := j.Load(path); err != nil {
+		return nil, err
+	}
+	return j, nil
+}
+
+func (j *JWT) Load(path string) error {
+	return jwt.NewConfig(path).Effect()
+}
+
+func (j *JWT) Sign(kid string, arg interface{}, minutes int) (string, error) {
+	return jwt.Sign(kid, arg, minutes)
+}
+
+func (j *JWT) Parse(token string) (interface{}, error) {
+	return jwt.Parse(token)
+}
+
+func (j *JWT) SignRequest(r *http.Request, kid string, arg interface{}, minutes int) error {
+	return jwt.SignRequest(r, kid, arg, minutes)
+}
+
+func (j *JWT) ParseRequest(r *http.Request) (interface{}, error) {
+	return jwt.ParseRequest(r)
+}
--- jwt.go
+++ jwt.go
@@ -4,8 +4,8 @@ import "net/http"
 
 type JWTToken interface {
 	Load(string) error
-	Sign(string, interface{}, int)
-	Parse(tokenString string) (interface{}, error)
+	Sign(string, interface{}, int) (string, error)
+	Parse(string) (interface{}, error)
 	SignRequest(*http.Request, string, interface{}, int) error
 	ParseRequest(*http.Request) (interface{}, error)
 }
