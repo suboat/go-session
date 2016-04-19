184ca9c6325f8ca60852a4441eee51d557ab7c79
WindomZ <zwdsdtc@gmail.com>
2016-04-19 17:57:39 +0800

    style: rename  session files

--- session.go
+++ session.go
@@ -15,3 +15,19 @@ type SessionStore interface {
 }
 
 const SessionKeyword string = "_session"
+
+type SessionCommonStore interface {
+	SessionStore
+	SetTag(*http.Request, http.ResponseWriter, string) error
+	GetTag(*http.Request) string
+	SetLevel(*http.Request, http.ResponseWriter, uint32) error
+	GetLevel(*http.Request) uint32
+	SetStatus(*http.Request, http.ResponseWriter, uint64) error
+	GetStatus(*http.Request) uint64
+}
+
+const (
+	SessionKeywordTag    string = "_tag"
+	SessionKeywordLevel         = "_level"
+	SessionKeywordStatus        = "_status"
+)
