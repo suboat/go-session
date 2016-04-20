72bd1d1b7466385e84c9af4dfb98efff28d6a102
WindomZ <zwdsdtc@gmail.com>
2016-04-20 10:54:28 +0800

    fix: jwt http const

--- jwt/http.go
+++ jwt/http.go
@@ -2,7 +2,7 @@ package gosession
 
 import "github.com/WindomZ/go-jwt/macro"
 
-const (
-	HEADER_KEY string = jwt.HEADER_KEY()          // Default: "Authorization"
-	HEADER_VALUE_PREFIX = jwt.HEADER_VALUE_PREFIX() // Default: "Bearer"
+var (
+	HEADER_KEY          string = jwt.HEADER_KEY()          // Default: "Authorization"
+	HEADER_VALUE_PREFIX        = jwt.HEADER_VALUE_PREFIX() // Default: "Bearer"
 )
